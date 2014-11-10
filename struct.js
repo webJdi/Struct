38fe1a31d27c5fe29d9ba1a801883a67b1e20b2b
Suman Senapati <sigma.6.2009@gmail.com>
2014-11-11 05:04:19 +0530

    Alpha Version 1.0.1

--- struct.js
+++ struct.js
@@ -5,12 +5,16 @@
 // This requires the latest jquery plugin
 $(document).ready(function(){
 	var offset=$('.affix').offset().top;
+	var leftoff=$('.affix').offset().left;
+	
 	$(window).scroll(function(){
 		var scrollTop=$(window).scrollTop();
 		
 		if (scrollTop>=offset)
 			{
+				$('.fixed').css("left",leftoff+"px");
 				$('.affix').addClass("fixed");
+				
 			}
 		else
 			{
